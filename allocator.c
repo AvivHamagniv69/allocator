c08e66a1e58eea9d17bb9c069143063b504f63f0
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2023-09-19 11:07:09 +0300

    Update and rename main.c to allocator.c

--- allocator.c
+++ allocator.c
@@ -3,23 +3,27 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "conf.h"
+#include "allocator.h"
 
-typedef struct block {
-    size_t size;
+typedef struct block BLOCK_ATTRIBUTES {
+    TYPEOF_SIZE size;
     bool used;
     struct block* next;
     intptr_t data[1]; // dont change placement, the struct always needs to be behind the data
 } block;
 
-static size_t align(size_t num) {
-    return (num + sizeof(intptr_t) - 1) & ~(sizeof(intptr_t) - 1);
-}
+#if IS_ALIGNED == true
+    static TYPEOF_SIZE align(TYPEOF_SIZE num) {
+        return (num + sizeof(intptr_t) - 1) & ~(sizeof(intptr_t) - 1);
+    }
+#endif
 
-static size_t alloc_size(size_t size) {
+static TYPEOF_SIZE alloc_size(TYPEOF_SIZE size) {
     return sizeof(block)+size-sizeof(intptr_t);
 }
 
-static block* req_mem(size_t size) {
+static block* req_mem(TYPEOF_SIZE size) {
     block* blck = (block*) sbrk(0);
 
     if (sbrk(alloc_size(size)) == (void*)-1) { // (void*)-1 is an error value that says that the memory cant be allocated
@@ -30,8 +34,14 @@ static block* req_mem(size_t size) {
 
 static block* header = NULL;
 
-void* alloc(size_t size) {
-    size = align(size);
+void* alloc(TYPEOF_SIZE size) {
+    if (size == 0) {
+        return NULL;
+    }
+
+    #if IS_ALIGNED == true
+        size = align(size);
+    #endif
     if (header == NULL) {
         block* b = req_mem(size);
         if (b == NULL) {
@@ -64,6 +74,16 @@ void* alloc(size_t size) {
     return b->data;
 }
 
+void memcpym(void* dest, const void* src, TYPEOF_SIZE size) {
+	char* d = dest;
+    const char* s = src;
+    while (size--) {
+        *d = *s;
+        d++;
+        s++;
+    }
+}
+
 void freem(void* ptr) {
     if (ptr == NULL) {
         return;
@@ -90,12 +110,33 @@ void freem(void* ptr) {
     tofree->used = false;
 }
 
-int main() {
+void* reallocm(void* ptr, TYPEOF_SIZE size) {
+    if (ptr == NULL || size == 0) {
+        return NULL;
+    }
+    
+    block* tofree = ptr-sizeof(block)+sizeof(intptr_t);
+    TYPEOF_SIZE amtocpy = size;
+    if (tofree->size < size) {
+        amtocpy = tofree->size;
+    }
+
+    void* temp = ptr;
+    ptr = alloc(size);
+    memcpym(ptr, temp, amtocpy);
+    freem(temp);
+
+    return ptr;
+}
+
+/*int main() {
     int* a = alloc(sizeof(int));
     *a = 69;
     int* b = alloc(sizeof(int));
     *b = 420;
+    int c = 1;
+    memcpyf(a, b, c);
     printf("%d\n", *a);
     freem(b);
     freem(a);
-}
+}*/
