a796341fca3197660b4217f7b512e9aa3d02a177
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2023-09-19 11:07:39 +0300

    Add files via upload

---
+++ allocator.h
@@ -0,0 +1,24 @@
+#ifndef ALLOCATOR
+#define ALLOCATOR
+
+#include "conf.h"
+
+#define memcpyf(dest, src, size) { \
+    __typeof__(dest) _dest = dest; \
+    __typeof__(src) _src = src; \
+    TYPEOF_SIZE _size = (TYPEOF_SIZE) size; \
+    while (_size) { \
+        *_dest = *_src; \
+        _dest++; \
+        _src++; \
+        _size--; \
+    } \
+}
+
+void* alloc(TYPEOF_SIZE size);
+
+void memcpym(void* dest, const void* src, TYPEOF_SIZE size);
+
+void freem(void* ptr);
+
+#endif
---
+++ conf.h
@@ -0,0 +1,3 @@
+#define BLOCK_ATTRIBUTES
+#define TYPEOF_SIZE size_t
+#define IS_ALIGNED true
